2370b23d44364d88fca0e8a16d8fda2023e59b8f
Patrick Davey <patrick.davey@gmail.com>
2016-10-29 18:35:06 +0100

    Add custom formatter for issues and dates
    This adds a custom formatter which pulls out
     #issue_DDD entries separately and makes a
    simple little bootstrap site for viewing them.
    
    It also shows a list timesheeting information
    broken out by month.

---
+++ timetrap/.timetrap/formatters/issues_dates.rb
@@ -0,0 +1,77 @@
+require 'erb'
+require_relative './new_text'
+require 'timetrap/formatters/csv'
+require 'active_support/core_ext/time/calculations.rb'
+
+class Issue
+  attr_reader :number, :entries
+
+  def initialize(issue_text, entries)
+    @number = issue_text.match(/(?<number>\d+)/)[:number].to_i
+    @entries = entries.select { |e| e.note.include?(issue_text) }
+  end
+
+  def timesheet
+    Timetrap::Formatters::NewText.new(entries).output
+  end
+
+  def title
+    "Issue #{number}"
+  end
+
+  def id
+    "issue_#{number}"
+  end
+
+  def <=>(other)
+    other.number <=> number
+  end
+end
+
+class Month
+  attr_reader :entries, :start, :finish
+
+  def initialize(month, entries)
+    @start = month
+    @finish = month.end_of_month
+    @entries = entries.select { |e| (start..finish).include?(e.start) }
+  end
+
+  def timesheet
+    Timetrap::Formatters::NewText.new(entries).output
+  end
+
+  def title
+    start.strftime("%Y-%b")
+  end
+
+  def id
+    start.strftime("month-%Y-%b")
+  end
+
+  def <=>(other)
+    other.start <=> start
+  end
+end
+
+class Timetrap::Formatters::IssuesDates
+  attr_reader :entries
+
+  def initialize(entries)
+    @entries = entries
+  end
+
+  def output
+    @complete_timesheet = Timetrap::Formatters::NewText.new(entries).output
+    complete_timesheet_csv = Timetrap::Formatters::Csv.new(entries).output
+    File.write("./timesheet.csv", complete_timesheet_csv)
+
+    renderer = ERB.new(File.read "/Users/patrickdavey/.timetrap/formatters/template.html.erb")
+    @unique_issues = @complete_timesheet.split.join("|").scan(/\bissue_\d{3,}\b/).uniq
+    @unique_issues = @unique_issues.map{|issue| Issue.new(issue, entries) }.sort
+    @months = entries.map { |e| e.start.beginning_of_month }.uniq
+    @months = @months.map{|month| Month.new(month, entries) }.sort
+    output = renderer.result(binding)
+    File.write("./index.html", output)
+  end
+end
