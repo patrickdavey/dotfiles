4fd86918acf5b2b8f11d6c15b774ffec81dec04f
Patrick Davey <patrick.davey@gmail.com>
2017-07-05 19:14:24 +1200

    Move timetrap into shared Dropbox folder
    Also mostly fix up the vim-style copy and paste from the tmux changes.

--- tmux/timesheet_checker.sh
+++ tmux/timesheet_checker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-RUNNING=`sqlite3 ~/.timetrap.db "select count(id) from entries where end IS NULL"`
+RUNNING=`sqlite3 ~/Dropbox/.timetrap.db "select count(id) from entries where end IS NULL"`
 
 if [ "$RUNNING" -eq "0" ]; then
   echo "#[fg=colour255,bg=colour1,bold] OFF"
